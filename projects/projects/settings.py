bc3e2dddc927f83b8467f56c5e6098a5fd8ebf0a
Kromag1995 <gonzalof95@hotmail.com>
2021-07-13 12:01:44 -0300

    Sacar secret key

--- projects/projects/settings.py
+++ projects/projects/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zm_$d0#h-q0(mga2s*a%@&*d2adzehe4^!5l(reay-m!290qx$'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
